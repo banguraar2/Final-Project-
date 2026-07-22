0d2d1c44b16b4c729ec43bf49f3e73a4a211c238
banguraar2 <banguraar2@vcu.edu>
2026-07-22 19:53:46 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,153 @@
+# Packages
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+# Page Configurations
+st.set_page_config(
+    page_title="Weekly Earnings Analysis",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+# Loading Data with efficiency (prevents streamlit from rereading data)
+@st.cache_data
+def load_data():
+    return pd.read_csv("fproject_earnings_data.csv")
+df = load_data()
+# Page customization
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #FFD1DF;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Widgets
+# Widget 1
+st.sidebar.header("Filters")
+edu = [
+    "All Education Groups"
+] + sorted(df["EDUC_GROUP"].dropna().unique().tolist())
+select_edu = st.sidebar.selectbox(
+    "Select Education Group:",
+    options=edu
+)
+# Widget 2
+select_sex = st.sidebar.radio(
+    "Select Gender:",
+    options=["All", "Male", "Female"],
+    horizontal=True
+)
+df_filter = df.copy() 
+if select_edu != "All Education Groups":
+  df_filter = df_filter[
+      df_filter["EDUC_GROUP"] == select_edu
+  ]
+if select_sex != "All":
+  df_filter = df_filter[
+      df_filter["SEX_LABEL"] == select_sex
+  ]
+# 1 Chart
+st.subheader("Weekly Earnings Distribution")
+earnings = df_filter["EARNWEEK2"].dropna()
+fig, ax = plt.subplots(figsize=(8,6))
+ax.hist(earnings, bins=30, color="hotpink", edgecolor="white")
+ax.set_title("Weekly Earnings Distributiion")
+ax.set_xlabel("Weekly Earnings")
+ax.set_ylabel("Number of Workers")
+ax.grid(axis="y", alpha=0.25)
+fig.tight_layout()
+st.pyplot(fig)
+plt.close(fig)
+# Explanation and text box customization
+st.markdown(
+    """
+    <div style="
+        background-color: #FFECF2;
+        color: #FF48B0;
+        padding: 12px 16px;
+        border-radius: 10px;
+        border-left: 6px solid #FF69B4;
+        margin-top: 10px;
+        margin-bottom: 24px;
+        font-size: 16px;
+     ">
+        <b>Chart Description:</b><br>
+        The histogram displays the distributed weekly earnings for all age groups, genders and education level. The chart can be filtered by education and gender.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+# Summary Statistics
+st.subheader("Summary Statistics Table")
+summary = (
+    df_filter["EARNWEEK2"]
+    .describe()
+    .rename(
+        {
+            "count": "Number of Workers",
+            "mean": "Mean",
+            "std": "Standard Deviation",
+            "min": "Minimum",
+            "25%": "Quartile 1",
+            "50%": "Median",
+            "75%": "Quartile 3",
+            "max": "Maximum"
+
+        }
+    )
+    .to_frame(name="Weekly Earnings")
+)
+summary["Weekly Earnings"] = (
+    summary["Weekly Earnings"].round(2)
+)
+# Customizing my summary table
+customize = (
+    summary.style
+    .format({"Weekly Earnings": "{:,.2f}"})
+    .set_table_styles([
+        {
+            "selector": "thead th",
+            "props":[
+                ("background-color", "#FF69B4"), # making the table header hot pink
+                ("color", "white"),
+                ("font-weight", "bold"),
+                ("text-align", "center")
+            ]
+        },
+        {
+            "selector": "tbody th",
+            "props": [
+                ("background-color", "#FF69B4"), # making the table labels hot pink
+                ("color", "white"),
+                ("font-weight", "bold")
+            ]
+        }
+    ])
+    .set_properties(**{
+        "background-color": "#FFE4EC", # making the table light pink
+        "color": "black"
+    })
+)
+st.table(customize) # commiting my customizations
+# Explanation and text box customization
+st.markdown(
+    """
+    <div style="
+        background-color: #FFECF2;
+        color: #FF48B0;
+        padding: 12px 16px;
+        border-radius: 10px;
+        border-left: 6px solid #FF69B4;
+        margin-top: 10px;
+        margin-bottom: 24px;
+        font-size: 16px;
+     ">
+        <b>Table Description:</b><br>
+        The table displays the statistical summary of the all age groups, education levels, and genders. The table can be filtered by education and gender.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
